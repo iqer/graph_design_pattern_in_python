e3a0860a47d535bb10a73b0446abcdb100a1ca00
chenqihang <chenqihang@imdada.cn>
2021-03-11 21:02:19 +0800

    add factory pattern

--- factory/product1.py
+++ factory/product1.py
@@ -11,8 +11,9 @@ class Factory(metaclass=ABCMeta):
     
     def create(self, owner):
         self.product = self.create_product(owner)
-        self.register_product(owner)
-    
+        self.register_product(self.product)
+        return self.product
+
     @abstractmethod
     def create_product(self, owner):
         pass
@@ -22,3 +23,37 @@ class Factory(metaclass=ABCMeta):
         pass
 
 
+class IDCard(Product):
+    def __init__(self, owner) -> None:
+        print(f'制作{owner}的ID卡')
+        self._owner = owner
+    
+    def use(self):
+        print(f'使用{self._owner}的ID卡')
+
+    def get_owner(self):
+        return self._owner
+
+
+class IDCardFactory(Factory):
+    def __init__(self) -> None:
+        self._owners = []
+
+    def create_product(self, owner):
+        return IDCard(owner)
+
+    def register_product(self, product):
+        self._owners.append(product.get_owner())
+    
+    def get_owners(self):
+        return self._owners
+
+
+if __name__ == '__main__':
+    factory = IDCardFactory()
+    card1 = factory.create('小明')
+    card2 = factory.create('小红')
+    card3 = factory.create('小刚')
+    card1.use()
+    card2.use()
+    card3.use()
